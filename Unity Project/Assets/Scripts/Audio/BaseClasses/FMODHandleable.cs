7536cb3813ef4f4c1572f02b4b7dcfa7973bff03
ShaunCayabyab <contact@shaun-cayabyab.com>
2019-02-23 18:22:35 -0500

    removing dropevent from FMODHandleable for now

--- Unity Project/Assets/Scripts/Audio/BaseClasses/FMODHandleable.cs
+++ Unity Project/Assets/Scripts/Audio/BaseClasses/FMODHandleable.cs
@@ -7,16 +7,8 @@ public class FMODHandleable : MonoBehaviour
     [FMODUnity.EventRef]
     public string GrabEvent;
 
-    [FMODUnity.EventRef]
-    public string DropEvent;
-
     public void PlayGrab()
     {
         FMODUnity.RuntimeManager.PlayOneShot(GrabEvent, gameObject.transform.position);
-    }
-
-    public void PlayDrop()
-    {
-        FMODUnity.RuntimeManager.PlayOneShot(DropEvent, gameObject.transform.position);
     }
 }
