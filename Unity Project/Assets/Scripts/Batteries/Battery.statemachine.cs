d74702e5f69300f8b6f6c1c31fa8094f6d97f0fd
ShaunCayabyab <contact@shaun-cayabyab.com>
2019-02-23 19:46:48 -0500

    hooking battery charge FMOD event

--- Unity Project/Assets/Scripts/Batteries/Battery.statemachine.cs
+++ Unity Project/Assets/Scripts/Batteries/Battery.statemachine.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using System;
 
 public partial class Battery : Lockable, Handleable.HandleableItem
@@ -12,6 +13,8 @@ public partial class Battery : Lockable, Handleable.HandleableItem
     BAD,
     DEAD
   }
+
+  public UnityEvent batteryChargeAudio;
 
   public float currentChargeInSeconds = 10;
   public float maxChargeInSeconds = 10;
@@ -50,6 +53,11 @@ public partial class Battery : Lockable, Handleable.HandleableItem
 
   protected bool AdjustCharge(float adjustment)
   {
+    if (adjustment > 0 && !charging)
+    {
+      batteryChargeAudio.Invoke();
+    }
+
     charging = (adjustment > 0);
     float attemptedCharge = this.currentChargeInSeconds + adjustment;
     float boundedByMin = Math.Max(attemptedCharge, 0);
