09755c4dc0a2a7831280b35d6f84b5fa415de326
TheFloatingBrain <TheFl@LAPTOP-UMJQFRCR>
2019-02-25 00:39:25 -0500

    Gravity disableing.

---
+++ Unity Project/Assets/Scripts/Engine/Engine.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Engine : MonoBehaviour
+{
+    [SerializeField] public DeviceBase engine;
+
+    protected bool previousState = true;
+
+    void Update()
+    {
+        if (engine.isActive == false)
+        {
+            Debug.Log("Engine not active!");
+            Rigidbody[] rigidBodies = Resources.FindObjectsOfTypeAll<Rigidbody>();
+            foreach (Rigidbody rigidbody in rigidBodies)
+                rigidbody.useGravity = false;
+            previousState = false;
+        }
+        else if (previousState == false )
+        {
+            Debug.Log("Engine on");
+            previousState = true;
+            Rigidbody[] rigidBodies = Resources.FindObjectsOfTypeAll<Rigidbody>();
+            foreach (Rigidbody rigidbody in rigidBodies)
+                rigidbody.useGravity = true;
+        }
+    }
+}
