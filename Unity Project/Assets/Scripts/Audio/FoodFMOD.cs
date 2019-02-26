85064c501ca19b2ae901e8b92caa6cbe99665830
ShaunCayabyab <Shaun.C397@gmail.com>
2019-02-26 18:08:13 -0500

    adding food FMOD script

---
+++ Unity Project/Assets/Scripts/Audio/FoodFMOD.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodFMOD : FMODHandleable
+{
+	[FMODUnity.EventRef]
+	public string foodEatEvent;
+
+	public void PlayFoodEating() {
+		FMODUnity.RuntimeManager.PlayOneShot(foodEatEvent);
+	}
+
+    // Start is called before the first frame update
+    void Start() {}
+
+    // Update is called once per frame
+    void Update() {}
+}
