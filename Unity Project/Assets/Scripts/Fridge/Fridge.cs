3a06ac47edee3b179e0566a8ecb638962efab978
Chris Wheeler <cmwhee@gmail.com>
2019-03-03 16:41:56 -0500

    fridge bugs fixed

--- Unity Project/Assets/Scripts/Fridge/Fridge.cs
+++ Unity Project/Assets/Scripts/Fridge/Fridge.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 using UnityEngine;
 
@@ -10,116 +11,84 @@ public class Fridge : MonoBehaviour
     public Lockable door;
 
     protected MeshRenderer yellowIndicatorLight, greenIndicatorLight;
-    protected Material yellowIndicatorLightMaterial, greenIndicatorLightMaterial;
+    
     protected DeviceBase refrigerator;
-    protected bool lastDoorState, doorIsClosed, foodIsPresent;
-    protected GameObject food;
-    public static readonly string GREEN_INDICATOR_LIGHT_RO = "indicator_light_green";
-    public static readonly string YELLOW_INDICATOR_LIGHT_RO = "indicator_light_yellow";
+    protected bool foodIsPresent;
+    
+    protected Food food;
+    
+    public static readonly string GREEN_INDICATOR_LIGHT_TAG = "green_indicator";
+    public static readonly string YELLOW_INDICATOR_LIGHT_TAG = "yellow_indicator";
     public static readonly string SHADER_KEYWORD_RO = "_EMISSION";
 
     void Start()
     {
         refrigerator = GetComponentInParent<DeviceBase>();
-        MeshRenderer[] childrenShaders = transform.parent.GetComponentsInChildren<MeshRenderer>();
-        Regex yellowLightRegex = new Regex(YELLOW_INDICATOR_LIGHT_RO);
-        Regex greenLightRegex = new Regex(GREEN_INDICATOR_LIGHT_RO);
-        foreach( MeshRenderer current in childrenShaders )
-        {
-            if (current.gameObject.name.CompareTo(YELLOW_INDICATOR_LIGHT_RO) == 0)
-            {
-                foreach (Material currentMaterial in current.materials) {
-                    if (yellowLightRegex.IsMatch( currentMaterial.name ) == true)
-                        yellowIndicatorLightMaterial = currentMaterial;
-                }
-                yellowIndicatorLight = current;
-            }
-            else if (current.gameObject.name.CompareTo(GREEN_INDICATOR_LIGHT_RO) == 0)
-            {
-                foreach (Material currentMaterial in current.materials) {
-                    if (greenLightRegex.IsMatch( currentMaterial.name ) == true)
-                        greenIndicatorLightMaterial = currentMaterial;
-                }
-                greenIndicatorLight = current;
-            }
-            if (greenIndicatorLight != null && yellowIndicatorLight != null)
-                break;
-        }
-        lastDoorState = door.IsLocked;
+        yellowIndicatorLight = this.GetComponentsInChildren<MeshRenderer>()
+            .First(x => x.CompareTag(YELLOW_INDICATOR_LIGHT_TAG));
+        
+        greenIndicatorLight = this.GetComponentsInChildren<MeshRenderer>()
+            .First(x => x.CompareTag(GREEN_INDICATOR_LIGHT_TAG));
+        
         foodIsPresent = false;
-        doorIsClosed = true;
+        
         refrigerator.ItemProduced += ProduceFood;
-        yellowIndicatorLightMaterial.DisableKeyword(SHADER_KEYWORD_RO);
-        greenIndicatorLightMaterial.DisableKeyword(SHADER_KEYWORD_RO);
     }
 
     void Update()
     {
-        if (doorIsClosed == true && ( foodIsPresent == false || food == null ) )
+        if(!door.IsLocked)
+            refrigerator.productionTime = 0;
+        
+        if (!foodIsPresent)
             refrigerator.hasItem = false;
-        else
-            refrigerator.hasItem = true;
-        if (refrigerator.isActive == true && doorIsClosed == true)
+        
+        if (refrigerator.isActive)
         {
-            if (foodIsPresent == true)
+            if (foodIsPresent)
             {
-                greenIndicatorLightMaterial.EnableKeyword(SHADER_KEYWORD_RO);
-                yellowIndicatorLightMaterial.DisableKeyword(SHADER_KEYWORD_RO);
+                greenIndicatorLight.materials[1].EnableKeyword(SHADER_KEYWORD_RO);
+                yellowIndicatorLight.materials[1].DisableKeyword(SHADER_KEYWORD_RO);
             }
             else
             {
-                greenIndicatorLightMaterial.DisableKeyword(SHADER_KEYWORD_RO);
-                yellowIndicatorLightMaterial.EnableKeyword(SHADER_KEYWORD_RO);
+                greenIndicatorLight.materials[1].DisableKeyword(SHADER_KEYWORD_RO);
+                yellowIndicatorLight.materials[1].EnableKeyword(SHADER_KEYWORD_RO);
             }
         }
         else {
-            yellowIndicatorLightMaterial.DisableKeyword(SHADER_KEYWORD_RO);
-            greenIndicatorLightMaterial.DisableKeyword(SHADER_KEYWORD_RO);
+            yellowIndicatorLight.materials[1].DisableKeyword(SHADER_KEYWORD_RO);
+            greenIndicatorLight.materials[1].DisableKeyword(SHADER_KEYWORD_RO);
         }
     }
 
     void OnDestroy() {
-        //Cleanup.//
         refrigerator.ItemProduced -= ProduceFood;
     }
 
     void ProduceFood(object sender, EventArgs e)
     {
-        food = GameObject.Instantiate(foodPrefab, 
+        var tmp = GameObject.Instantiate(foodPrefab, 
                 foodOrienter.transform.position, foodOrienter.transform.rotation);
+        
+        food = tmp.GetComponent<Food>();
+        
         door.Unlock();
     }
     
-    void OnTriggerStay(Collider other)
+    void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.GetComponent<Lockable>() == door)
         {
-            doorIsClosed = true;
-            if (refrigerator.isActive == true)
-            {
-                if (foodIsPresent == true)
-                    door.Unlock();
-                else
-                    door.Lock();
-            }
-            else
-            {
-                if (lastDoorState == true)
-                    door.Lock();
-                else
-                    door.Unlock();
-            }
-            lastDoorState = door.IsLocked;
+            if(!foodIsPresent) door.Lock();
         }
-        else if (other.gameObject.GetComponent<Food>() != food && food != null)
+        else if (other.gameObject.GetComponent<Food>() == food )
             foodIsPresent = true;
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.GetComponent<Food>() != food && food != null)
+        if (other.gameObject.GetComponent<Food>() == food)
             foodIsPresent = false;
-        if (other.gameObject.GetComponent<Lockable>() == door)
-            doorIsClosed = false;
     }
 }
