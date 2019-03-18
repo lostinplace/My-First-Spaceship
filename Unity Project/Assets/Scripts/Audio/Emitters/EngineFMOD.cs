b92a860c125b18e93cd235f39dbe112abaa91a0f
ShaunCayabyab <Shaun.C397@gmail.com>
2019-03-18 19:00:14 -0400

    running engine FMOD emitter

--- Unity Project/Assets/Scripts/Audio/Emitters/EngineFMOD.cs
+++ Unity Project/Assets/Scripts/Audio/Emitters/EngineFMOD.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class EngineFMOD : FMODEmitter
 {
-    // Start is called before the first frame update
-    void Start() {}
-
     // Update is called once per frame
-    void Update() {}
+    void Update()
+    {
+        if (playerState && !playerState.EngineIsActive)
+        {
+            base.setOffline();
+        }
+        else
+        {
+            base.setOnline();
+        }
+    }
 }
